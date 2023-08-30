883b4e8f3899a3e1a1a354a59845c2e1cc78a80e
Flat_cat <92849778+Flat-Cat@users.noreply.github.com>
2023-08-30 14:50:56 +0200

    Implemented RESTful Api

---
+++ backend/src/main/java/com/project/backend/controller/BookController.java
@@ -0,0 +1,69 @@
+package com.project.backend.controller;
+import com.project.backend.repository.BookRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.project.backend.entity.Book;
+import com.project.backend.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @ sind wie hooks
+@RestController
+@RequestMapping("/api/v1")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public List < Book > getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity < Book > getBookById(@PathVariable(value = "id") Long bookId )
+            throws ResourceNotFoundException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId ));
+        return ResponseEntity.ok().body(book);
+    }
+    @PostMapping("/books")
+    public Book createBook(@Valid @RequestBody Book book ) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity < Book > updateBook(@PathVariable(value = "id") Long bookId,
+                                              @Valid @RequestBody Book bookDetails) throws ResourceNotFoundException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
+
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setISBN(bookDetails.getISBN());
+        book.setPubYear(bookDetails.getPubYear());
+        final Book updatedBook = bookRepository.save(book);
+        return ResponseEntity.ok(updatedBook);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public Map < String, Boolean > deleteBook(@PathVariable(value = "id") Long bookId)
+            throws ResourceNotFoundException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
+
+        bookRepository.delete(book);
+        Map < String, Boolean > response = new HashMap < > ();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
---
+++ backend/src/main/java/com/project/backend/entity/Book.java
@@ -0,0 +1,64 @@
+package com.project.backend.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    private long id;
+    private String title;
+    private String author;
+    private int pub_year;
+    private String isbn;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int pub_year, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.pub_year = pub_year;
+        this.isbn = isbn;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+//____________title_______________________________________________________________________________
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+//____________title_______________________________________________________________________________
+    @Column(name = "title", nullable = false)
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title ) {
+        this.title= title;
+    }
+//____________author_______________________________________________________________________________
+    @Column(name = "author", nullable = false)
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author ) {
+        this.author = author;
+    }
+//____________pub_year_______________________________________________________________________________
+    @Column(name = "pub_year", nullable = false)
+    public int getPubYear() {
+        return pub_year;
+    }
+    public void setPubYear(int pubYear ) {
+        this.pub_year = pubYear;
+    }
+//____________isbn_______________________________________________________________________________
+    @Column(name = "isbn", nullable = false)
+    public String getISBN() {
+        return isbn;
+    }
+    public void setISBN(String isbn ) {
+        this.isbn = isbn;
+    }
+}
