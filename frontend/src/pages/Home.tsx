c9a06e5c2a5bd25179e3f9d79d5fdbf0ee0d1dd8
Flat_cat <92849778+Flat-Cat@users.noreply.github.com>
2023-09-19 14:05:48 +0200

    added grid and LinkTo

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -16,42 +16,47 @@ function Home() {
     return (
 
         <div className="container">
+            <div className="cardGrid">
 
-            {data.map((item, index) => (
+                {data.map((item, index) => (
+                    <Link to={`/book/${item.id}`}>
+                   
+                    <div className="card" key={index}>
+                        <div className="card-header">
 
-                <div className="card mx-6 my-6">
+                            {/* ___________Placeholder Image___________ */}
+                            <div className="card-image">
+                                <figure className="image is-16x24">
+                                    <img src="https://image-placeholder.com/images/actual-size/240x400.png" alt="Placeholder" />
+                                </figure>
+                            </div>
 
-                    <div className="card-header">
-
-                        {/* ___________Placeholder Image___________ */}
-                        <div className="card-image mr-1">
-                            <figure className="image is-16x24">
-                                <img src="https://image-placeholder.com/images/actual-size/240x400.png" alt="Placeholder" />
-                            </figure>
-                        </div>
-
-                        {/* ___________Card-Content___________ */}
-                        <div className="card-content">
-                            <div className="media" key={index}>
-                                <div className="media-content">
-                                    <p className="subtitle is-6 has-text-left mx-5 my-5">{item.author} </p>
-                                    <p className="title is-4 has-text-left mx-5 my-1">{item.title}</p>
-                                    <p className="subtitle is-6 has-text-left mx-5 my-5">{item.pubYear} </p>
+                            {/* ___________Card-Content___________ */}
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content">
+                                        <p className="subtitle is-6 has-text-left has-text-grey mx-5 my-2">{item.author}</p>
+                                        <p className="title is-4 has-text-left mx-5 my-1">{item.title}</p>
+                                        <p className="subtitle is-6 has-text-left has-text-grey-light mx-5 my-5">{item.pubYear}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
+                        {/* ___________Card-Footer___________ */}
+                        <footer className="card-footer">
+                            <p className="card-footer-item">
+                                <span>
+                                    <span className="has-text-grey-light">ISBN: </span> {item.isbn}</span>
+                            </p>
+                        </footer>
+
                     </div>
+                    </Link>
+                ))}
+                
+            </div>
 
-                    {/* ___________Card-Footer___________ */}
-                    <footer className="card-footer">
-                        <p className="card-footer-item">
-                            <span>
-                                <span className="has-text-grey-light">ISBN: </span> {item.isbn}</span>
-                        </p>
-                    </footer>
-
-                </div>
-            ))}
         </div>
     )
 }
